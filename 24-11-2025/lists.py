4e9536c69245809f8f3a759b0f7be09c4ce71178
Shubham Namasudra <silviosudra@gmail.com>
2025-11-24 17:25:50 +0530

    Create lists.py

---
+++ 24-11-2025/lists.py
@@ -0,0 +1,17 @@
+x=[10, 20, 30, 40, 50]
+print(x)
+
+fruits=["cherry", "apple", "tomato"]
+print(fruits[0])
+print(fruits[1])
+print(fruits[-1])
+
+l=[]
+for i in range(1, 21):
+    l.append(i)
+print(l)
+for i in l:
+    if i%2==0:
+        print(i, " is even.")
+    else:
+        print(i, " is odd.")
