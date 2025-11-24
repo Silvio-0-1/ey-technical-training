6e8b8c8f637ad99dc4e010db62631ee37960e92f
Shubham Namasudra <silviosudra@gmail.com>
2025-11-24 17:41:59 +0530

    Update lists.py

--- 24-11-2025/lists.py
+++ 24-11-2025/lists.py
@@ -1,11 +1,14 @@
+# creating and printing a list
 x=[10, 20, 30, 40, 50]
 print(x)
 
+# list indexing
 fruits=["cherry", "apple", "tomato"]
 print(fruits[0])
 print(fruits[1])
 print(fruits[-1])
 
+# list traversal
 l=[]
 for i in range(1, 21):
     l.append(i)
@@ -15,3 +18,16 @@ for i in l:
         print(i, " is even.")
     else:
         print(i, " is odd.")
+
+# list operations
+fruits=["cherry", "apple", "tomato"]
+print(fruits)
+
+fruits[1]="kiwi"
+print(fruits)
+
+fruits.append("mango")
+print(fruits)
+
+fruits.insert(1, "banana")
+print(fruits)
