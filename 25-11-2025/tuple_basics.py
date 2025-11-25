29ec04be012b05d654ff0fc00569937db8baca11
Shubham Namasudra <silviosudra@gmail.com>
2025-11-25 12:33:38 +0530

    Create tuple_basics.py

---
+++ 25-11-2025/tuple_basics.py
@@ -0,0 +1,10 @@
+numbers = (10, 20, 30)
+names = ("Alan", "Rahul", "Neha")
+
+mixed = (10, "hello", 3.5, True)
+
+fruits = ("apple", "banana", "kiwi")
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[-1])
