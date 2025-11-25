e80d567188d38fb6beb6f80814103f07d6380a26
Shubham Namasudra <silviosudra@gmail.com>
2025-11-25 14:45:07 +0530

    Create sets_operations.py

---
+++ 25-11-2025/sets_operations.py
@@ -0,0 +1,20 @@
+a = {1, 2, 3}
+b = {3, 4, 5}
+
+# Union
+print(a | b)
+
+# Intersection
+print(a & b)
+
+# Difference
+print(a - b)
+
+# Symmetric Difference
+print(a ^ b)
+
+# Output
+# {1, 2, 3, 4, 5}
+# {3}
+# 1, 2}
+# {1, 2, 4, 5}
