63ae83902c463438550610576f12745ac8c8db8d
Shubham Namasudra <silviosudra@gmail.com>
2025-11-25 12:33:57 +0530

    Create tuple_operations.py

---
+++ 25-11-2025/tuple_operations.py
@@ -0,0 +1,11 @@
+nums = (1, 2, 2, 3)
+print(nums.count(2))
+
+letters = ('a', 'b', 'c', 'd')
+print(letters.index('c'))
+
+nums = (10, 20, 30, 40, 50)
+
+print(nums[1:4])
+print(nums[:3])
+print(nums[::-1])
