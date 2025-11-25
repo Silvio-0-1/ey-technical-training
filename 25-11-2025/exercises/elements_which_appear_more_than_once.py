69e38659e34f673bb6ea090ed131519ca39eb21d
Shubham Namasudra <silviosudra@gmail.com>
2025-11-25 15:35:51 +0530

    Create elements_which_appear_more_than_once.py

---
+++ 25-11-2025/exercises/elements_which_appear_more_than_once.py
@@ -0,0 +1,14 @@
+li = [1, 2, 3, 2, 4, 1, 5, 1]
+li2 = []
+dict = {}
+
+for i in li:
+    if i in dict:
+        dict[i] += 1
+    else:
+        dict[i] = 1
+ 
+for i, j in dict.items():
+    if j > 1:
+        li2.append(i)
+print(li2)
