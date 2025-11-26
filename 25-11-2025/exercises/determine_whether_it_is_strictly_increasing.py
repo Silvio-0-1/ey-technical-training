d57fc4666092c80b77666a10c79734f943e66d8b
Shubham Namasudra <silviosudra@gmail.com>
2025-11-26 09:36:25 +0530

    Update and rename determine_whether_it_is_strictly_increasing to determine_whether_it_is_strictly_increasing.py

--- 25-11-2025/exercises/determine_whether_it_is_strictly_increasing.py
+++ 25-11-2025/exercises/determine_whether_it_is_strictly_increasing.py
@@ -1,12 +1,10 @@
-numbers = (1, 3, 5, 7, 9)
+numbers = (1, 3, 5, 4, 7, 9)
 
 is_increasing = True
 
 for i in range(0, len(numbers)-1):
     if numbers[i] > numbers[i+1]:
         is_increasing = False
+        break
 
-if is_increasing:
-    print("Increasing")
-else:
-    print("Not increasing")
+print("Strictly Increasing" if is_increasing else "Not strictly increasing")
