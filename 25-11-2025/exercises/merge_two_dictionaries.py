31463cf73308c3bf3e4f4cd5664eb609bf95a25f
Shubham Namasudra <silviosudra@gmail.com>
2025-11-26 10:44:38 +0530

    Update and rename merge_two_dictionaries to merge_two_dictionaries.py

--- 25-11-2025/exercises/merge_two_dictionaries.py
+++ 25-11-2025/exercises/merge_two_dictionaries.py
@@ -4,16 +4,14 @@ li = []
 dict3 = {}
 
 for i, j in dict1.items():
-    if i in dict3:
-        dict3[i]=list(dict3[i],j)
-    else:
         dict3[i]= j
 
 for i, j in dict2.items():
     if i in dict3:
-        li.append(dict3[i])
-        li.append(j)
-        dict3[i]=li
+        if type(dict3[i]) == list:
+            dict3[i].append(j)
+        else:
+            dict3[i]=[dict3[i],j]
     else:
         dict3[i]= j       
         
