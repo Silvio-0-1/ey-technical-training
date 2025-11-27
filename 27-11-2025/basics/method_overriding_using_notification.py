59fc714ba3bf704a16f7f1df05f7786b325931d4
Shubham Namasudra <silviosudra@gmail.com>
2025-11-27 12:28:46 +0530

    Create method_overriding_using_notification.py

---
+++ 27-11-2025/basics/method_overriding_using_notification.py
@@ -0,0 +1,25 @@
+class Notification:
+    def send(self, message):
+        print("Sending notification:", message)
+
+class EmailNotification(Notification):
+    def send(self, message):
+        print("Sending EMAIL to user:", message)
+
+class SMSNotification(Notification):
+        def send(self, message):
+            print("Sending SMS to user:", message)
+
+class PushNotification(Notification):
+        def send(self, message):
+            print("Sending PUSH notification:", message)
+
+# Usage
+n1 = EmailNotification()
+n1.send("Your order is confirmed")
+
+n2 = SMSNotification()
+n2.send("Your OTP is 4589")
+
+n3 = PushNotification()
+n3.send("You have a new message")
