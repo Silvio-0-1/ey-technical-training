1641d0c138d393e8c95765bc0587fe51452b555c
Silvio <silviosudra@gmail.com>
2025-12-06 01:12:11 +0530

    updates

---
+++ 04-12-2025/exercises/module_6.sql
@@ -0,0 +1,25 @@
+--MODULE 6: MySQL CRUD & Joins (Full Backend) (11 Exercises)
+
+
+--40. Create a database retail_app and two tables: customers and orders. Insert 10 rows.
+CREATE DATABASE retail_app;
+USE retail_app;
+
+
+
+--41. Write SOL: fetch all customers who placed more than 2 orders.
+--42. Write SOL:
+--list products that were never ordered.
+--43. Write SOL:
+--total amount spent by each customer.
+--44. Write SOL: orders of each customer including customers with zero orders (left join).
+--all products and match orders even if no sale occurred (right join).
+--45. Write SOL:
+--46. Write SQL
+--: find customers ordering from multiple categories.
+--47. Write SOL: list top 3 highest revenue orders.
+--48. Write SOL:
+--detect missing customer-id or product-id in orders.
+--generate a report of (customer x month x total amount).
+--49. Write SOL:
+--50. Write a SQL query using CROSS JOIN to generate all (customer x product) combinations.
