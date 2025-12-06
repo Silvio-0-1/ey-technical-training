a37e6cb02d2dd664e2b460422cb94dac9d16f331
Silvio <silviosudra@gmail.com>
2025-12-06 19:39:38 +0530

    updates

---
+++ 05-12-2025/date_time_manipulation/ex_6_calculate_age.py
@@ -0,0 +1,21 @@
+# 16. Calculate a user's precise age (years, months, days) based on DOB.
+
+from datetime import datetime, timedelta, date
+
+def calculate_age(dob, today = date.today()):
+    years = today.year - dob.year
+    months = today.month - dob.month
+    days = today.day - dob.day
+
+    if days < 0:
+        months -= 1
+        days +=30
+
+    if months < 0:
+        years -= 1
+        months +=12
+
+    return f"{years} years, {months} months, {days} days"
+
+dob = datetime.strptime(input("Enter date of birth: "), "%Y-%m-%d").date()
+print("Precise age: ", calculate_age(dob))
